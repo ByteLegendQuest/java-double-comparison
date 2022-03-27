55f6d6f241f61989c76c60ecb28bf60e4d9159ed
yunghf <yung.hf@outlook.com>
2022-03-27 03:49:47 +0000

    Update at 2022-03-27T03:49:47.012294Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,12 +1,12 @@
 package com.bytelegend;
 
 public class Main {
-    static final float Threshold = 0.00001f;
+    static final float THRESHOLD = 0.00001f;
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return (Math.abs(a-b) < Threshold);
+        return (Math.abs(a-b) < THRESHOLD);
     }
 }
