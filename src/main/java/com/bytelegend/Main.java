e1ab023a9322e7c2f6fb1879372e7c8859304620
SN-Beaux <Beaux.Luo@supernovacompanies.biz>
2022-03-31 08:05:10 +0000

    Update at 2022-03-31T08:05:10.888901Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,11 +1,11 @@
 package com.bytelegend;
 
-public class Main {
+public class IntegerOverflow {
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        return a - b < 0.00001;
     }
 }
