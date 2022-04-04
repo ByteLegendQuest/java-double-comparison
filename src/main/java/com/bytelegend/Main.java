2780948d9acd430e1b94e36f6f8716f55b1a8499
denghaoze <57385221+denghaoze@users.noreply.github.com>
2022-04-04 01:39:36 +0000

    Update at 2022-04-04T01:39:36.901846Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,13 +1,13 @@
 package com.bytelegend;
 
 public class Main {
-    private final static double EPSILON = 0.00001;
+    private final static double EPSILON = 0.00000001;
 
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return (a - b) < EPSILON;
+        return (a - b) <= EPSILON;
     }
 }
