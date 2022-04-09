63b0189f4bc671cc816dd080d95edef9f8a4112b
liubaolai <1164552059@qq.com>
2022-04-09 14:29:06 +0000

    Update at 2022-04-09T14:29:05.998768Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,11 +1,13 @@
 package com.bytelegend;
 
 public class Main {
+    private static final double EPSION = 0.0000001;
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        double epsilon = a > b ? a - b : b - a;
+        return epsilon < EPSION;
     }
 }
