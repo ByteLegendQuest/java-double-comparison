d2df44bf2804532e5472cc9238b70d9d471cc081
hesitatemore <1299136605@qq.com>
2022-04-09 13:28:48 +0000

    Update at 2022-04-09T13:28:48.377802Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
+
     public static boolean doubleEquals(double a, double b) {
         if ((a - b) > 0) {
             return (a - b) < EPSILON;
