efe880b299a1863a47519a3af434177b45aa2e23
1994xiong <xiongmengxm@163.com>
2022-04-10 09:22:12 +0000

    Update at 2022-04-10T09:22:12.714836Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -3,11 +3,12 @@ package com.bytelegend;
 import static java.lang.Double.valueOf;
 
 public class Main {
+    public static double EPSILON = 0.0000001f;
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return new BigDecimal(valueOf(a)).equals(new BigDecimal(valueOf(b)));
+        return a == b?true:Math.abs(a-b)<EPSILON;
     }
 }
