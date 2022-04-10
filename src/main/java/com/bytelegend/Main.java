f6cacc058a7b1a0426f1fad0e4a4f7be8987767d
1994xiong <xiongmengxm@163.com>
2022-04-10 09:12:23 +0000

    Update at 2022-04-10T09:12:23.525981Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
+        System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1f + 0.2f, 0.3f));
     }
 
     public static boolean doubleEquals(double a, double b) {
