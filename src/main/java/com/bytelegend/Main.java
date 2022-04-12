77f100055c72063e006085927ca28e88cf9f587c
MegaMidnight <richarr916@gmail.com>
2022-04-12 04:44:48 +0000

    Update at 2022-04-12T04:44:48.434028Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,11 +1,12 @@
 package com.bytelegend;
 
 public class Main {
+    public static final double EPSILON = 1e-10;
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        return Math.abs(a - b) < EPSILON;
     }
 }
