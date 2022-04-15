cbaa1a5ea8bad73f63aef8a5d9c2c108acb2959d
g497813927 <admin@techzjc.com>
2022-04-15 07:57:50 +0000

    Update at 2022-04-15T07:57:50.902181Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,11 +1,12 @@
 package com.bytelegend;
 
 public class Main {
+    private static final double EPSILON = 2e-52;
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        return Math.abs(a - b) < EPSILON;
     }
 }
