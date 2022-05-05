bb11b8098afe2f41dba1721dc9ea5c699366213f
michealmachine <micheal0machine@gmail.com>
2022-05-05 14:37:59 +0000

    Update at 2022-05-05T14:37:59.675436Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -6,7 +6,7 @@ public class Main {
     }
 
     public static boolean doubleEquals(double a, double b) {
-        if(Math.abs(a-b)>0.001)return false;
+        if(Math.abs(a-b)>0.000001)return false;
         else return true;
     }
 }
