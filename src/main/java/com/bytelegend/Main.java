ded8d2102e4e1227dbc19d43514c4842303db4a0
Yeely0162 <Yeely01621@163.com>
2022-05-12 12:36:41 +0000

    Update at 2022-05-12T12:36:41.848741Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,11 +1,14 @@
 package com.bytelegend;
-
+import java.text.DecimalFormat;
 public class Main {
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
-    public static boolean doubleEquals(double a, double b) {
-        return a == b;
+   public static boolean doubleEquals(double a, double b) {
+        DecimalFormat df = new DecimalFormat("#0.00000000");
+        String x = df.format(a);
+        double y = Double.parseDouble(x);
+        return y == b ;
     }
 }
