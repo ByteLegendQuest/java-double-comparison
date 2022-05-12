61eb19787b1c09f6b6e9132b7eef7b16f426f013
Yeely0162 <Yeely01621@163.com>
2022-05-12 16:04:58 +0000

    Update at 2022-05-12T16:04:58.924295Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -6,6 +6,9 @@ public class Main {
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        DecimalFormat df = new DecimalFormat("#0.00000000");
+        String x = df.format(a);
+        double y = Double.parseDouble(x);
+        return y == b;
     }
 }
