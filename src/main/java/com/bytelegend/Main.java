51c905863049483089eae00362f40c4b9b820447
Longyang-Liu <2452791011@qq.com>
2022-05-12 00:33:43 +0000

    Update at 2022-05-12T00:33:43.575060Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -6,6 +6,11 @@ public class Main {
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        if(a - b < 0.001) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
