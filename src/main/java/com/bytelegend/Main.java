b958dad052d73401d18b2ff1d7810dbecfceae15
Yeely0162 <Yeely01621@163.com>
2022-05-12 16:06:27 +0000

    Update at 2022-05-12T16:06:27.840387Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
-    public static boolean doubleEquals(double a, double b) {
+     public static boolean doubleEquals(double a, double b) {
         DecimalFormat df = new DecimalFormat("#0.00000000");
         String x = df.format(a);
         double y = Double.parseDouble(x);
