fc471ecf6ec0c4f1358002c666533c7b275adc67
andrewbaxter439 <andy@andybaxter.me>
2022-05-28 10:40:47 +0000

    Update at 2022-05-28T10:40:47.792128Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -6,6 +6,6 @@ public class Main {
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return Math.abs(a - b) < (a + b) / 1_000_000;
+        return Math.abs(a - b) < (a + b) / 1_000_000_000;
     }
 }
