dde4fc20b5c1f9e97bb5c9e383cf6f1ad89b8d1b
andrewbaxter439 <andy@andybaxter.me>
2022-05-28 10:34:37 +0000

    Update at 2022-05-28T10:34:37.967441Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,11 +1,13 @@
 package com.bytelegend;
 
+import Math;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return abs(a - b) < (a + b)/1_000_000;
+        return Math.abs(a - b) < (a + b) / 1_000_000;
     }
 }
