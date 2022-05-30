e7ab5b548f25816e9a4fd1bd27372dde563a0470
chinahzh <2987140244@qq.com>
2022-05-30 01:28:06 +0000

    Update at 2022-05-30T01:28:06.559114Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,11 +1,16 @@
 package com.bytelegend;
-
+//
 public class Main {
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        float temp = a - b;
+        if (temp<soa)
+            return True;
+        else
+            return Flase;
+
     }
 }
