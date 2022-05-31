09c91c21625936c02391f79e6688e343b7631d33
s9x <rich@xtinction.org>
2022-05-31 23:41:03 +0000

    Update at 2022-05-31T23:41:03.465576Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,11 +1,13 @@
 package com.bytelegend;
 
-public class Main {
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        if ( a - b < .001) {
+            return true;
+        } else {
+            return false;
+        }
     }
-}
