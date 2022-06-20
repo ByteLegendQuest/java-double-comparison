f74e0792ccf5fd193793ee3f23fdcc50ab99d8f4
karolk75 <karolk410410@gmail.com>
2022-06-20 11:04:46 +0000

    Update at 2022-06-20T11:04:46.618594Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,5 +1,7 @@
 package com.bytelegend;
 
+import java.lang.Math.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
