95727383440d18d86867450bd073a77640256282
batman405 <2638603343@qq.com>
2022-06-23 05:55:14 +0000

    Update at 2022-06-23T05:55:14.282713Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,11 +1,13 @@
 package com.bytelegend;
 
 public class Main {
+    private final static double EPSILON = 0.00001;
+
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b ? true : Math.abs(a - b) < 0.00001;
+        return a == b ? true : Math.abs(a - b) < EPSILON;
     }
 }
