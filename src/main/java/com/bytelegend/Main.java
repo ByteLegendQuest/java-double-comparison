d367967e52d0af0e3ef625b7b9b155a6b5c5ff27
2809769075 <2809769075@qq.com>
2022-07-01 02:28:22 +0000

    Update at 2022-07-01T02:28:22.305075Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,4 +1,4 @@
-package com.bytelegend;
+   package com.bytelegend;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,6 +6,11 @@ public class Main {
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        float num = 0.001;
+        if(Match.abs((a-b)< num)) {
+            return ture;
+        }else{
+            return flase;
+        }
     }
 }
