d624d5077320c91c0d3ddf401da3aa8b6a1d5f69
shiguoqing99 <3118482822@qq.com>
2022-07-01 14:21:18 +0000

    Update at 2022-07-01T14:21:18.727447Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -6,6 +6,5 @@ public class Main {
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
-    }
-}
+        double num = 0.00000000001
+        return ((a - b) < num );
