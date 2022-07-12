9e89bc1ed20fe87fa9ccdb9b18fc1c5321424c12
chesterbachelor <kawalerbartosz@gmail.com>
2022-07-12 22:24:17 +0000

    Update at 2022-07-12T22:24:17.778807Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -5,13 +5,8 @@ public class Main {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
-    public static boolean doubleEquals(double a, double b) {
-        double EPSILON = 0.00001;
-        if (a > b) {
-            return (a - b) < EPSILON;
-        }
-        else if(b > a) {
-            return (b - a) < EPSILON;
-        }
+    public static boolean doubleEquals(double a, double b) {       
+        double num = 0.00000000001;
+        return Math.abs (a - b) < num;
     }
 }
