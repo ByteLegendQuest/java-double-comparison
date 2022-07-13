481464e45007affb144b1aef5cae0e06a2270393
Birlinha <omenaribeiro13@gmail.com>
2022-07-13 16:30:06 +0000

    Update at 2022-07-13T16:30:06.011899Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,21 +1,12 @@
 package com.bytelegend;
 
 public class Main {
-    public static Float a;
-    public static Float b;
-    public static Float c;
-    public static Float d;
-    public static Float e;
-
     public static void main(String[] args) {
-        c = 0.1;
-        d = 0.2;
-        e = c + d ;
-
-        System.out.println("0.1+0.2==0.3: " + floatEquals(e, 0.3));
+        System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
-    public static boolean floatEquals(float a, float b) {
-        return a == b;
+    public static boolean doubleEquals(double a, double b) {
+        double epsilon = 0.00000000001; 
+        return Math.abs ( a - b ) < epsilon;
     }
 }
