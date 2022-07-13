ec330f907b709a9a880e34b34403aacb8ca5bafd
Birlinha <omenaribeiro13@gmail.com>
2022-07-13 16:44:45 +0000

    Update at 2022-07-13T16:44:45.695347Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -5,8 +5,9 @@ public class Main {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
+    public static double epsilon = Double.MIN_VALUE;
+
     public static boolean doubleEquals(double a, double b) {
-        double epsilon = Double.MIN_VALUE;
         return Math.abs ( a - b ) < Math.abs( epsilon );
     }
 }
