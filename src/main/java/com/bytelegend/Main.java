612c96bb5a894816619c3ec63dfec2cf9f1ee8d4
GOH73 <goh73@outlook.com>
2022-07-17 03:31:42 +0000

    Update at 2022-07-17T03:31:42.054789Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,12 +1,11 @@
 package com.bytelegend;
-
-public class Main {
-    public static void main(String[] args) {
+public class MaxValues {
+	public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        double epsilon = 0.00001;
-        return a - b < epsilon;
+        double epsilon = 0.00000000001;
+        return Math.abs(a - b) < epsilon;
     }
 }
