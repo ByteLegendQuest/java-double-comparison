5587fdf12c45de0cdfa83b09148bfa80b72411de
TOHSAKA-RINN <1911872724@qq.com>
2022-07-22 13:28:12 +0000

    Update at 2022-07-22T13:28:11.996590Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,16 +1,18 @@
 package com.bytelegend;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
         double epsilon = 1e-10;
-        if(a - b < epsilon){
+
+        if(Math.abs(a-b) < epsilon){
            return true;
        }else {
            return false;
-       }    
+       }
     }
 }
