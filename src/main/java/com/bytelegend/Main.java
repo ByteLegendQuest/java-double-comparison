f2427d2f68f9a3c6acb36503660fcff762ed8bfa
lpkyyds <2065870265@qq.com>
2022-08-15 09:06:42 +0000

    Update at 2022-08-15T09:06:42.908219Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,6 +7,6 @@ public class Main {
 
     public static boolean doubleEquals(double a, double b) {
         double epsilon = 0.0000000001;
-        if (Math.abs(a - b) < epsilon)
+        return Math.abs(a - b) < epsilon;
     }
 }
