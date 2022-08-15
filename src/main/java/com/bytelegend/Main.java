77a35cfd0008b2d0aa5808781cd3de8139f7b33e
lpkyyds <2065870265@qq.com>
2022-08-15 09:05:46 +0000

    Update at 2022-08-15T09:05:46.558607Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -6,6 +6,7 @@ public class Main {
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        double epsilon = 0.0000000001;
+        if (Math.abs(a - b) < epsilon)
     }
 }
