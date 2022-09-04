4a93894ac3efd5ccce2f14472198d8870aac4135
ZusmanFate <demonsLwg@foxmail.com>
2022-09-04 03:04:19 +0000

    Update at 2022-09-04T03:04:19.737940Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -6,6 +6,10 @@ public class Main {
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        double ex = a - b;
+        if (ex < 0.000000000000001 && ex > -0.000000000000001) {
+            return true;
+        }
+        return false;
     }
 }
