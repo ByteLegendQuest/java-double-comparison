ea32e9017af34726f62672a4f16082ba41703880
Mooos-MoSheng <1297393910@qq.com>
2022-10-10 10:19:20 +0000

    Update at 2022-10-10T10:19:20.214075Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -5,7 +5,8 @@ public class Main {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
+    public static double eps = 0.000000000001;
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        return -eps <= a - b && a - b <= eps;
     }
 }
