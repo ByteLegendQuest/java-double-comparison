8754c33b4c74d0e664e78b8fb0abf7e85cacfde4
tiger0961 <29515547@qq.com>
2022-10-12 05:45:55 +0000

    Update at 2022-10-12T05:45:55.646418Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -6,6 +6,6 @@ public class Main {
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a - b < 0.0000000001 || a - b > -0.00001;
+        return Math.abs(a - b) < 1e-6;
     }
 }
