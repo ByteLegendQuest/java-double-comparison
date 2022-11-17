b93c0dce52e948dce5d622593924ce40dfaf5355
jufjzq <136180840@qq.com>
2022-11-17 01:55:06 +0000

    Update at 2022-11-17T01:55:06.765725Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -1,12 +1,12 @@
 package com.bytelegend;
 
 public class Main {
+    private final static double EPSILON = 0.000000001;
     public static void main(String[] args) {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
     public static boolean doubleEquals(double a, double b) {
-        double EPSILON = 0.00001;
         return Math.abs(a - b) < EPSILON;
     }
 }
