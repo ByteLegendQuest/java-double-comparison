e97cf87a1c4ce7367200b4a07b24bbfbb4ccd9f5
jenny-2233 <822204051@qq.com>
2022-11-27 03:25:34 +0000

    Update at 2022-11-27T03:25:34.733815Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -5,7 +5,13 @@ public class Main {
         System.out.println("0.1+0.2==0.3: " + doubleEquals(0.1 + 0.2, 0.3));
     }
 
-    public static boolean doubleEquals(double a, double b) {
-        return a == b;
+     public static boolean doubleEquals(double a, double b) {
+        if(a-b<0.000001){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
+       
+
