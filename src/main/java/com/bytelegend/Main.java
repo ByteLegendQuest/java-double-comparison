86288600483f50fd8423169e894c47d6efa90db1
johnsonxiaoyu <1272304153@qq.com>
2022-11-30 03:12:32 +0000

    Update at 2022-11-30T03:12:32.152569Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -6,6 +6,15 @@ public class Main {
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        public static boolean doubleEquals(double a, double b) {
+        Float aFloat = new Float(a);
+        Float bFloat = new Float(b);
+        int i = aFloat.compareTo(bFloat);
+        Boolean flag =false;
+        if(i == 0){
+            flag = true;
+        }
+        return flag;
+    }
     }
 }
