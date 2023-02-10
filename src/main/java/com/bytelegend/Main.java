4e93c58362b862ad10bd7afc5cb34a66ea8ea4e3
Ajieoooo <1823451579@qq.com>
2023-02-10 14:21:20 +0000

    Update at 2023-02-10T14:21:20.916516Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -6,7 +6,7 @@ public class Main {
     }
 
     public static boolean doubleEquals(double a, double b) {
-        if (Math.abs(a-b) =< 0.000000001) {
+        if (Math.abs(a-b) =< 1e-6) {
             return true;
         }
         else {
