6c0394d990cef7e01f670d01e437f20e4ef51227
Windsority <mcfengxin@gmail.com>
2023-06-04 21:40:23 +0000

    Update at 2023-06-04T21:40:23.841598Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -6,6 +6,7 @@ public class Main {
     }
 
     public static boolean doubleEquals(double a, double b) {
-        return a == b;
+        if (a > b) return (a - b) < 0.000001;
+        return (b - a) < 0.000001;
     }
 }
